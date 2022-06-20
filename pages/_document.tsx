124d648c7077cccd34cf0b9128871b4c441b5085
yusung <smilejjou@gmail.com>
2022-06-20 17:58:28 +0900

    ing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,9 +8,10 @@ export default class RootDocument extends Document {
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" />
           <meta name="description" content="Dev.log"/>
-          <meta name="keywords" content="blog,react,antd,webpack,css,javascript" />
+          <meta name="keywords" content="blog,react,webpack,css,javascript" />
           {/* <link rel="manifest" href="/static/manifest.json" />
           <link rel="shortcut icon" href="/static/favicon.ico" /> */}
+          <link rel="shortcut icon" href="/public/favicon.png" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/notosanskr.css" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/styles/railscasts.min.css" />
---
+++ pages/api/menu.tsx
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const menu = () => {
+  return '';
+}
+export default menu;
