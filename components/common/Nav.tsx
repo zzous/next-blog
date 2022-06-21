b0847a96d036644408b039cd8d6ed6ab00151a7b
yusung <smilejjou@gmail.com>
2022-06-21 15:36:31 +0900

    netlify build test

--- components/common/Nav.tsx
+++ components/common/Nav.tsx
@@ -1,32 +1,6 @@
-import path from 'path';
-
 import { AiFillCaretUp, AiFillCaretDown } from 'react-icons/ai';
 import Link from 'next/link'
 const Header = () => {
-  // const fs = require('fs');
-  // function getStaticProps() {
-  //   let files = fs.readdirSync('pages');
-  //   files = files.filter((file) => {
-  //     if (file == '_app.js') return false;
-  //     const stat = fs.lstatSync(file);
-  //     return stat.isFile();
-  //   });
-
-  //   const pages = files.map((file) => {
-  //     const link = path.parse(file).name;
-  //     const name = link;
-  //     return {
-  //       name: name,
-  //       link: link,
-  //     };
-  //   });
-
-  //   return {
-  //       props: {
-  //           pages: pages,
-  //       },
-  //   };
-  // }
   return (
     <div id='nav'>
       <div className='search'>
